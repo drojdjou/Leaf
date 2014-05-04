7bc07e7d9eacdeb3cdb091a4164763a32abad051
Bartek Drozdz <bartek@everyday3d.com>
2014-05-03 23:31:58 -0700

    Added physical object, changed particle demo

--- html/demo/08_particles.js
+++ html/demo/08_particles.js
@@ -1,34 +1,48 @@
 var addParticle = function(x, y) {
-	var p = new Leaf.Sphere([1,1,1,0.5]);
+	var p = new Leaf.Particle([1,1,1,0.5]);
 
-	var ox = (100 + Math.random() * 200) - 150;
-	var t = 3600 + 1600 * Math.random();
-	var f = 40 + 120 * Math.random();
-	var ft = 800;
+	// var ox = (100 + Math.random() * 200) - 150;
+	// var t = 3600 + 1600 * Math.random();
+	// var f = 40 + 120 * Math.random();
+	// var ft = 800;
 
-	p.position
-		.init(x, y)
-		.to(x + ox * 0.33, y - f)
-		.in(ft, Leaf.Ease.sineOut)
-		.to(x + ox, y + 200)
-		.in(t, Leaf.Ease.quadIn)
+	// p.position
+	// 	.init(x, y)
+	// 	.to(x + ox * 0.33, y - f)
+	// 	.in(ft, Leaf.Ease.sineOut)
+	// 	.to(x + ox, y + 200)
+	// 	.in(t, Leaf.Ease.quadIn)
+	// ;
+
+	// p.radius
+	// 	.init(5)
+	// 	.to(0)
+	// 	.in(t + ft)
+	// ;
+
+	p.force
+		.init(0, 0)
+		.to(Math.random() * 2 - 1, -(Math.random() + 1))
+		.in(100)
+		.to(0, 0)
+		.in(100)
 	;
 
-	p.radius
-		.init(5)
-		.to(0)
-		.in(t + ft)
-	;
+	p.setStartPosition(x, y);
+
+	p.radius.init(5).to(0).in(1000);
 
 	leaf.add(0, p);
 }
 
-var mx, my;
-var ppf = 2;
+var mx = 0, my = 0;
+var ppf = 5, making = false;
 
 document.addEventListener('mousemove', function(e) {
 	mx = e.pageX;
 	my = e.pageY;
+
+	making = true;
 });
 
 document.addEventListener('touchmove', function(e) {
@@ -36,11 +50,37 @@ document.addEventListener('touchmove', function(e) {
 	my = e.pageY;
 });
 
+document.addEventListener('touchstart', function(e) {
+	making = true;
+});
+
+document.addEventListener('touchend', function(e) {
+	making = false;
+});
+
 
 var makeParticles = function() {
 	requestAnimationFrame(makeParticles);
 
-	for(var i = 0; i < ppf; i++) addParticle(mx, my);
+	if(making) {
+		for(var i = 0; i < ppf; i++) addParticle(mx, my);
+	}
 }
 
 makeParticles();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ html/src/objects/Particle.js
@@ -0,0 +1,46 @@
+Leaf.Particle = function(color) {
+
+	Leaf.Particle2d.call(this);
+
+	var that = this;
+	var fullCircle = Math.PI * 2;
+
+	this.stroke = false;
+	this.dash = false;
+	this.strokeSize = p_(1);
+
+	this.name = "sphere";
+	this.radius = p_(100);
+	this.color = p_.apply(null, color || [1,1,1,1]);
+
+	this.draw = function(t, rn, an) {
+		var cx, r, c, sw;
+
+		cx = rn._2d.context;
+		r = an.radius.get(t);
+		c = an.color.get(t);
+
+		sw = an.strokeSize.get(t)[0];
+		
+		if(that.dash) {
+			var circ = 2 * Math.PI * r;
+			var d = circ / that.dash;
+			cx.setLineDash([d]);
+		}
+
+		cx.globalAlpha = c[3];
+		cx.lineWidth = sw;
+
+		if(that.stroke) cx.strokeStyle = Leaf.Color.floatArray2Css(c);
+		else cx.fillStyle = Leaf.Color.floatArray2Css(c);
+
+		cx.beginPath();
+		cx.arc(0, 0, r, 0, fullCircle);
+		
+		if(that.stroke) cx.stroke();
+		else cx.fill();
+	}
+} 
+
+Leaf.Particle.prototype = new Leaf.Particle2d();
+Leaf.Particle.prototype.constructor = Leaf.Particle2d;
